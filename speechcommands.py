acf0c4a66c88d2f2f6b2c721b7e420b9b08ef610
Ahmad M <ahmedmansoor60@gmail.com>
2017-12-22 10:42:35 +0500

    speech

---
+++ speechcommands.py
@@ -0,0 +1,33 @@
+"""
+                                       
+                o  8                   
+                8  8                   
+.oPYo. o    o  o8P 8oPYo. .oPYo. odYo. 
+8    8 8    8   8  8    8 8    8 8' `8 
+8    8 8    8   8  8    8 8    8 8   8 
+8YooP' `YooP8   8  8    8 `YooP' 8   8 
+8 ....::....8 ::..:..:::..:.....:..::..
+8 :::::::ooP'.:::::::::::::::::::::::::
+..:::::::...:::::::::::::::A7MD0V:::::::
+::::Python Learning::::::::::::15:03:17:::
+::::::;; Speech Commands ;;:::::::::::::::
+"""
+
+# Download SR: pip install SpeechRecognition PyAudio
+
+# Implement PyAutoGUI for speech to commands
+                                                                                         
+import speech_recognition as sr  
+
+# get real time audio from the microphone                                                                       
+r = sr.Recognizer()                                                                                   
+with sr.Microphone() as source:                                                                       
+    print("Speak:")                                                                                   
+    audio = r.listen(source)   
+
+try:
+    print("You said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
