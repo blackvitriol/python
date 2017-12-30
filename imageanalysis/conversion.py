cce0eae8ce312a483be9b73af9e9c62a025dfeeb
Ahmad M <ahmed_mansoor_60@hotmail.com>
2017-12-30 20:21:23 +0500

    Update conversion.py

--- imageanalysis/conversion.py
+++ imageanalysis/conversion.py
@@ -21,24 +21,50 @@ what kind of operations we should apply to them.
 import numpy as np
 import cv2
 from tkinter.filedialog import askopenfilename
-from skimage import colour
+from tkinter.messagebox import showinfo
+from skimage import color
+from skimage import data
 from matplotlib import pyplot as plt
+
+showinfo("A7's Image Conversion", "Welcome! This is version 0.1 alpha, so select your image then pls close the windows to view alternatives. You can also save images.")
 
 # Load the file
 loadedfile = askopenfilename()
+img = data.load(loadedfile)
 
 # Convert to different styles
-img = cv2.imread(loadedfile,0)
+img_hsv = color.rgb2hsv(img)
+img_yuv = color.rgb2yuv(img)
+img_ycbcr = color.rgb2ycbcr(img)
+img_lab = color.rgb2lab(img)
 
-img_hsv = skimage.color.rgb2hsv(img)
-img_yuv = skimage.color.rgb2yuv(img)
-img_ycbcr = skimage.color.rgb2ycbcr(img)
-img_lab = skimage.color.rgb2lab(img)
-
-
-# View the image
-plt.imshow(cv_img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.title('Is this the best color?', color='#afeeee')
+# View the image regular
+plt.imshow(img)
+plt.title('RGB Image', color='black')
+plt.axis("off")
 plt.show()
 
+# View the HSV image (Hue, Saturation, Value)
+plt.imshow(img_hsv)
+plt.title('HSV Image', color='black')
+plt.axis("off")
+plt.show()
+
+# View the YUV image
+plt.imshow(img_yuv)
+plt.title('YUV Image', color='black')
+plt.axis("off")
+plt.show()
+
+# View the Y-Cb-Cr image
+plt.imshow(img_ycbcr)
+plt.title('Y-Cb-Cr Image', color='black')
+plt.axis("off")
+plt.show()
+
+# View the Lab image
+plt.imshow(img_lab)
+plt.title('Lab Image', color='black')
+
+plt.axis("off")
+plt.show()
