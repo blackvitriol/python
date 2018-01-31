51ee83c17792127ab0a93f7b927c64ac0167f06e
Ahmad M <ahmed_mansoor_60@hotmail.com>
2018-01-31 08:31:39 +0500

    Create pythontricks.py

---
+++ pythontricks.py
@@ -0,0 +1,8 @@
+# just adding some tricks here for referencing later
+
+# create loading animations
+import sleep, tqdm
+size = 100
+for _i in tqdm.tqdm(range(size)):
+    time.sleep(0.5)
+    size = 2+2
