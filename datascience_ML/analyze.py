71785c09a73a13e3b1e68c32691c099ffa93c431
Ahmad M <ahmed_mansoor_60@hotmail.com>
2018-02-06 12:56:35 +0500

    Update analyze.py
    numpy added

--- datascience_ML/analyze.py
+++ datascience_ML/analyze.py
@@ -4,13 +4,18 @@ load your variable as sample
 '''
 
 sample = lbl
-
 print(id(sample))
 print(type(sample))
 print(len(sample))
 print(repr(sample))
 
 if type(sample).__module__ == "numpy":
-    print("The variable selected is a Numpy Array with size: ",sample.size, )
+    print("This is a Numpy N-Dimension Array with",'\n'
+          "size:",sample.size,'\n'
+          "shape:",sample.shape,'\n'
+          "strides:",sample.strides,'\n'
+          "data type:", sample.dtype,'\n'
+          "flags:",'\n', sample.flags
+         )
     
 print("this is a work in progress")
