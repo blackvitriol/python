253f34382b4ef5e396125be8699e2302b6242bc9
Ahmad M <ahmed_mansoor_60@hotmail.com>
2018-02-13 10:14:53 +0500

    Update pythontricks.py

--- pythontricks.py
+++ pythontricks.py
@@ -1,7 +1,10 @@
 # just adding some tricks here for referencing later
+try:
+    import json, sleep, tqdm
+except ImportError:
+    warnings.warn("Some libraries are missing from your Python..check the requirements.txt")
 
 # create loading animations
-import sleep, tqdm
 size = 100
 for _i in tqdm.tqdm(range(size)):
     time.sleep(0.5)
